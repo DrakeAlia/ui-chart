47c136ea1c4839570d2a3efc3057c2d9d37d225a
Drake Alia <52472741+DrakeAlia@users.noreply.github.com>
2024-08-22 12:27:27 -0700

    finished ui chart with custom chart colors

--- src/components/Charts.tsx
+++ src/components/Charts.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { Bar, BarChart, CartesianGrid, XAxis } from "recharts";
+import { Bar, BarChart, CartesianGrid, LineChart, XAxis } from "recharts";
 
 import {
   ChartConfig,
@@ -11,6 +11,29 @@ import {
   ChartLegendContent,
 } from "@/components/ui/chart";
 
+import { Monitor } from "lucide-react";
+
+// Chart data 1
+// const chartData = [
+//   { browser: "chrome", visitors: 187, fill: "var(--color-chrome)" },
+//   { browser: "safari", visitors: 200, fill: "var(--color-safari)" },
+// ];
+
+// const chartConfig = {
+//   visitors: {
+//     label: "Total Visitors",
+//   },
+//   chrome: {
+//     label: "Chrome",
+//     color: "hsl(var(--chart-1))",
+//   },
+//   safari: {
+//     label: "Safari",
+//     color: "hsl(var(--chart-2))",
+//   },
+// } satisfies ChartConfig;
+
+// Chart data 2
 const chartData = [
   { month: "January", desktop: 186, mobile: 80 },
   { month: "February", desktop: 305, mobile: 200 },
@@ -23,11 +46,19 @@ const chartData = [
 const chartConfig = {
   desktop: {
     label: "Desktop",
-    color: "#2563eb",
+    // icon: Monitor,
+    color: "hsl(var(--chart-1))",
+    // color: "#2563eb",
+    // OR
+    // theme: {
+    //   light: "#2563eb",
+    //   dark: "#dc2626",
+    // },
   },
   mobile: {
     label: "Mobile",
-    color: "#60a5fa",
+    // color: "#60a5fa",
+    color: "hsl(var(--chart-2))",
   },
 } satisfies ChartConfig;
 
@@ -35,6 +66,7 @@ export function Charts() {
   return (
     <ChartContainer config={chartConfig} className="min-h-[200px] w-full">
       <BarChart accessibilityLayer data={chartData}>
+        {/* <LineChart accessibilityLayer /> */}
         <CartesianGrid vertical={false} />
         <XAxis
           dataKey="month"
