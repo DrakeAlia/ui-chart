a31746aae0316df89b0c22280d0f127286e065db
Drake Alia <52472741+DrakeAlia@users.noreply.github.com>
2024-09-03 12:38:38 -0700

    even better margin

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,7 +5,7 @@ import { PieChartWheel } from "@/components/PieChartWheel";
 
 export default function Home() {
   return (
-    <div className="h-[calc(100vh-5rem)] sm:h-auto sm:min-h-[600px]">
+    <div className="mt-3 h-[calc(100vh-5rem)] sm:h-auto sm:min-h-[600px]">
         <CoffeeChart />
     </div>
   );
